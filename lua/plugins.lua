a6ec352bc55e3d2e8efcde30b4df41e6361b4ed0
Tsunetsuki <l.paal@aol.de>
2024-09-30 17:31:03 +0900

    python virtual env support

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -108,4 +108,26 @@ return {
             require("autoclose").setup()
         end,
     },
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        lazy = false,
+        branch = "regexp", -- This is the regexp branch, use this for the new version
+        config = function()
+            require("venv-selector").setup({
+                settings = {
+                    options = {
+                        notify_user_on_venv_activation = true,
+                    }
+                }
+            })
+        end,
+        keys = {
+            { ",v", "<cmd>VenvSelect<cr>" },
+        },
+    },
 }
