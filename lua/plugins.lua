3f0e156296cbf14b825fe8e23b434f09f60ddc8e
Tsunetsuki <l.paal@aol.de>
2024-10-03 01:18:52 +0900

    surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -175,5 +175,12 @@ return {
         config = function()
             require('Comment').setup()
         end,
+    },
+    {
+        "kylechui/nvim-surround",
+        config = function()
+            require('nvim-surround').setup()
+        end,
+
     }
 }
