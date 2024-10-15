e5ed826779f0009955942424a825c25492cf5412
Tsunetsuki <l.paal@aol.de>
2024-10-15 14:03:49 +0900

    harpoon no longer uses telescope ui

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -226,8 +226,12 @@ return {
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            local harpoon = require('harpoon')
-            harpoon:setup({})
+            local harpoon = require("harpoon")
+            harpoon:setup({
+                settings = {
+                    save_on_toggle = true,
+                }
+            })
 
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
@@ -240,45 +244,6 @@ return {
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
             vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
-            -- basic telescope configuration
-            local conf = require("telescope.config").values
-            local function toggle_telescope(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
-                end
-                local finder = function()
-                    local paths = {}
-                    for _, item in ipairs(harpoon_files.items) do
-                        table.insert(paths, item.value)
-                    end
-
-                    return require("telescope.finders").new_table({
-                        results = paths,
-                    })
-                end
-                require("telescope.pickers").new({}, {
-                    prompt_title = "Harpoon",
-                    finder = finder(),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
-                    attach_mappings = function(prompt_bufnr, map)
-                        map("i", "<C-d>", function()
-                            local state = require("telescope.actions.state")
-                            local selected_entry = state.get_selected_entry()
-                            local current_picker = state.get_current_picker(prompt_bufnr)
-
-                            table.remove(harpoon_files.items, selected_entry.index)
-                            current_picker:refresh(finder())
-                        end)
-                        return true
-                    end,
-                }):find()
-            end
-
-            vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-                { desc = "Open harpoon window" })
         end,
     },
     {
