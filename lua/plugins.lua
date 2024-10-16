28569c5eb1cce795fc6b0345e015f39f3ba45f63
Tsunetsuki <l.paal@aol.de>
2024-10-17 00:57:50 +0900

    autosession

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -348,5 +348,17 @@ return {
         config = function()
             vim.keymap.set("n", "<leader>i", require("lspimport").import, { noremap = true })
         end,
+    },
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+            -- log_level = 'debug',
+        }
     }
 }
