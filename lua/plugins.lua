38df54c9908baaf0a75aa5803005653b442b292b
Tsunetsuki <l.paal@aol.de>
2024-11-18 11:52:55 +0900

    minor

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -362,6 +362,7 @@ return {
 
         ---enables autocomplete for opts
         ---@module "auto-session"
+        ---@diagnostic disable-next-line: undefined-doc-name
         ---@type AutoSession.Config
         opts = {
             suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
