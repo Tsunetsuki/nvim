956a3982d2d3d686178ca4535e7455a2b1e2940b
Tsunetsuki <l.paal@aol.de>
2024-11-20 11:47:53 +0900

    minor (changed order of plugins)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -394,39 +394,6 @@ return {
     --     end
     -- },
     {
-        "benlubas/molten-nvim",
-        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-        build = ":UpdateRemotePlugins",
-        init = function()
-            -- this is an example, not a default. Please see the readme for more configuration options
-            vim.g.molten_output_win_max_height = 12
-            -- activate the following line to change neovim's python env, but be careful that this env include ALL necessary packages, even those required by other neovim plugins!
-            -- vim.g.python3_host_prog = vim.fn.expand("C:\\Users\\lpaal\\miniconda3\\envs\\neovim\\python")
-            vim.keymap.set("n", "<localleader>ip", function()
-                local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
-                print(venv)
-                if venv ~= nil then
-                    -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-                    venv = string.match(venv, "/.+/(.+)")
-                    vim.cmd(("MoltenInit %s"):format(venv))
-                else
-                    vim.cmd("MoltenInit python3")
-                end
-            end, { desc = "Initialize Molten for python3", silent = true })
-
-            vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>",
-                { silent = true, desc = "Initialize the plugin" })
-            vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
-                { silent = true, desc = "run operator selection" })
-            vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>",
-                { silent = true, desc = "evaluate line" })
-            vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>",
-                { silent = true, desc = "re-evaluate cell" })
-            vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
-                { silent = true, desc = "evaluate visual selection" })
-        end,
-    },
-    {
         "folke/trouble.nvim",
         opts = {}, -- for default options, refer to the configuration section for custom setup.
         cmd = "Trouble",
@@ -463,5 +430,37 @@ return {
             },
         },
     },
+    {
+        "benlubas/molten-nvim",
+        version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+        build = ":UpdateRemotePlugins",
+        init = function()
+            -- this is an example, not a default. Please see the readme for more configuration options
+            vim.g.molten_output_win_max_height = 12
+            -- activate the following line to change neovim's python env, but be careful that this env include ALL necessary packages, even those required by other neovim plugins!
+            -- vim.g.python3_host_prog = vim.fn.expand("C:\\Users\\lpaal\\miniconda3\\envs\\neovim\\python")
+            vim.keymap.set("n", "<localleader>ip", function()
+                local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+                print(venv)
+                if venv ~= nil then
+                    -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+                    venv = string.match(venv, "/.+/(.+)")
+                    vim.cmd(("MoltenInit %s"):format(venv))
+                else
+                    vim.cmd("MoltenInit python3")
+                end
+            end, { desc = "Initialize Molten for python3", silent = true })
 
+            vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>",
+                { silent = true, desc = "Initialize the plugin" })
+            vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
+                { silent = true, desc = "run operator selection" })
+            vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>",
+                { silent = true, desc = "evaluate line" })
+            vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>",
+                { silent = true, desc = "re-evaluate cell" })
+            vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+                { silent = true, desc = "evaluate visual selection" })
+        end,
+    }
 }
