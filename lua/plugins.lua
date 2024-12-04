82223946b5c7b35ac9551d8eaedd6549d259211f
Tsunetsuki <l.paal@aol.de>
2024-12-04 16:31:30 +0900

    vimtex with sumatrapdf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -462,5 +462,21 @@ return {
             vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
                 { silent = true, desc = "evaluate visual selection" })
         end,
-    }
+    },
+    {
+        "lervag/vimtex",
+        lazy = false, -- we don't want to lazy load VimTeX
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+
+            -- vim.g.vimtex_view_general_viewer = 'okular'
+            -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+
+            vim.g.vimtex_view_general_viewer = 'sumatraPDF'
+            vim.g.vimtex_view_general_options = '-reuse-instance @pdf'
+            -- vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
+            -- vim.g.vimtex_view_method = "sumatrapdf"
+        end
+    },
 }
