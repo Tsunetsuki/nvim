9f08d9b5588ba58ed6ec991528145f6a685f9ab4
Tsunetsuki <l.paal@aol.de>
2025-09-23 12:55:38 +0200

    no colema

--- lua/remap.lua
+++ lua/remap.lua
@@ -75,10 +75,6 @@ end)
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 
--- better for colemak
-vim.keymap.set({'n', 'v', 'o'}, 'm', "j")
-
-
 -- quickly navigate quickfix lists
 vim.keymap.set('n', ']e', '<Cmd>try | cnext | catch | cfirst | catch | endtry<CR>')
 vim.keymap.set('n', '[e', '<Cmd>try | cprevious | catch | clast | catch | endtry<CR>')
